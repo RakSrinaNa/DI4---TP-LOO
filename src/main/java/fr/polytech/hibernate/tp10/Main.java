5d3b84dd33863b67f88ae02cb3dbf797f0c9039b
Thomas COUCHOUD <zerderr@gmail.com>
2017-11-20 16:19:29 +0100

    TP10 Main

---
+++ src/main/java/fr/polytech/hibernate/tp10/Main.java
@@ -0,0 +1,15 @@
+package fr.polytech.hibernate.tp10;
+
+/**
+ * Created by Thomas Couchoud (MrCraftCod - zerderr@gmail.com) on 20/11/2017.
+ *
+ * @author Thomas Couchoud
+ * @since 2017-11-20
+ */
+public class Main
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
